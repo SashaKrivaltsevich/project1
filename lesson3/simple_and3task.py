a994670fc6dfea83d8ec8a81c53e34a87ea585ba
SashaKrivaltsevich <122094905+SashaKrivaltsevich@users.noreply.github.com>
2023-01-15 16:37:06 +0300

    work with простыми операторами и 3 задание

---
+++ lesson3/simple_and3task.py
@@ -0,0 +1,40 @@
+# int
+int_ = 18
+int_2 = 23
+print(int_ + int_2)
+print(int_ - int_2)
+print(int_ * int_2)
+print(int_ / int_2)
+print(int_ // int_2) # целая часьт от деления
+print(int_ % int_2) # остаток от деления
+print(-int_)
+print( abs(int_))
+print(int_ ** 2)
+print(pow(int_, int_2))
+print(int_.bit_count())
+print(int_.bit_length())
+print(int_.to_bytes())
+
+# str
+str_ = 'bla'
+str_2 = 'belarus'
+print(str_ + str_2)
+print(str_ * 3)
+print(len(str_))
+print(str_[0])
+print(str_2[2:4])
+print(str_2[2:-3])
+print(str_2[:3])
+print(str_2[2:])
+print(str_2[3])
+print(str_.upper)
+print(str_2.capitalize())
+
+# bool
+bool_ = True
+bppl_2 = False
+
+# float
+float_ = 12.57
+float_2 = 67.0
+print((float_ + float_2))
