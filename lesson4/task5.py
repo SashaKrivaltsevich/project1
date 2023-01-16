7d4ff53a7b1990c9c04ec1e50f61bcaf444780d9
SashaKrivaltsevich <122094905+SashaKrivaltsevich@users.noreply.github.com>
2023-01-16 23:00:05 +0300

    проверка двух чисед на больше, меньше, равно

---
+++ lesson4/task5.py
@@ -0,0 +1,9 @@
+x = int(input('first num:'))
+y = int(input('second num:'))
+
+if x == y:
+    print('Числа равны')
+elif x > y:
+    print('Первое число больше')
+else:
+    print('первое число меньше')
